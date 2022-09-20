aba511fb2d075f1cba34f2e2b4b64cb6de45d7e7
Askar Aziz <103616025+askaraziz@users.noreply.github.com>
2022-09-20 21:02:40 +0700

    Create soal3.java

---
+++ PRAKTIKUM 2/soal3.java
@@ -0,0 +1,10 @@
+public class Prak3Soal3
+{
+public static void main(String arg[])
+{
+   for (int i=0; i<5; i++){
+   for (int j=4; j>=i; j--)
+     System.out.print('*');
+     System.out.println();}
+ }
+} 
