f401e27038dc3754ada5f2516e65ef2950981b3d
Askar Aziz <103616025+askaraziz@users.noreply.github.com>
2022-09-20 21:03:52 +0700

    Create soal5.java

---
+++ PRAKTIKUM 2/soal5.java
@@ -0,0 +1,10 @@
+public class soal5 {
+
+    public static void main(String[] args) {
+    
+    for (int i=0; i<=20; i=i+2){
+    if (i%6 ==0)
+    continue;
+    System.out.println(i); }
+     }
+    }
