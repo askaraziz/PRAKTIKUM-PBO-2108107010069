c4d05ee5da44da332e762e7d26a0406d0e76c37e
Askar Aziz <103616025+askaraziz@users.noreply.github.com>
2022-09-20 21:01:30 +0700

    Create soal1.java

---
+++ PRAKTIKUM 2/soal1.java
@@ -0,0 +1,12 @@
+public class Prak3Soal1 {
+    public static void main(String args[]) {
+       float a = 3;
+       float b = 2;
+       
+       System.out.println("a + b = " + (a + b));
+       System.out.println("a - b = " + (a - b));
+       System.out.println("a * b = " + (a * b));
+       System.out.println("a / b = " + (a / b));
+       System.out.println("a % b = " + (a % b));
+     }
+    }
